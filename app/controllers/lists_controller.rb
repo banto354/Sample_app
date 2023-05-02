2b9662fca90a772f396963912dc0a8793f2c7696
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-05-02 07:06:42 +0000

    [Finish] 4-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,18 @@
 class ListsController < ApplicationController
   def new
+    #Viewに渡すためのインスタント変数に空のオブジェクトを生成する
+    @list = List.new
   end
-
+  
+  def create
+    #データ受け取り作成のためのインスタンス作成
+    list = List.new(list_params)
+    #データベース保存のためのsaveメソッド
+    list.save
+    #トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+  
   def index
   end
 
@@ -9,5 +20,11 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  #ストロングパラメーター
+  def list_params
+    params.require(:list).permit(:title,:body)
   end
 end
